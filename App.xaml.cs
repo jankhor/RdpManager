d5e64d3b54de4d8cfd4f553bf9e8bcdd3af3bbaf
Cheng Yeh <cheng.yeh@cubic.com>
2025-04-12 15:59:51 -0700

    Resolved Run At Windows Startup not starting up.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows; // For WPF Application
 using System.Windows.Forms; // For NotifyIcon (but we won't use Forms.Application)
+using System.IO;
+using System.Reflection;
 
 namespace RdpManager
 {
@@ -8,6 +10,11 @@ namespace RdpManager
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+
+            // Set the current working directory to where the executable is, for the executable to
+            // be invoked from any folder and still resolve all the DLL.
+            Environment.CurrentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+
             // Single instance check
             var mutex = new System.Threading.Mutex(true, "RdpManager", out bool createdNew);
             if (!createdNew)
