c100bc4a43aa2e146976e4bd5fc16f52a6460b31
Cheng Yeh <cheng.yeh@cubic.com>
2025-04-22 22:56:39 -0700

    Fix Icon now showing up on the top-level menu items and remove the already running check, it is not needed.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,7 +11,6 @@ using Serilog;
 using Serilog.Sinks.File;  // Add this for RollingInterval
 using Serilog.Context;
 using System.Runtime.InteropServices;  // Add this line
-using MessageBox = System.Windows.MessageBox;
 
 namespace RdpManager {
     public partial class MainWindow : Window {
@@ -22,9 +21,6 @@ namespace RdpManager {
         private Image? connectionImage=null;
         private Image? webImage=null;
         private Image? defaultScImage=null;
-
-        // 1. Add this to prevent multiple instances
-        private static Mutex _mutex = new Mutex(true, "{8F6F0AC4-B9A1-45FD-A8CF-72F04E6BDE8F}");
 
         private readonly ILogger _logger;
 
@@ -40,15 +36,6 @@ namespace RdpManager {
                 ).CreateLogger();
 
             _logger = Log.ForContext<MainWindow>();
-            _viewModel = new MainViewModel();
-
-            // 2. Check for existing instance
-            if (!_mutex.WaitOne(TimeSpan.Zero, true)) {
-                MessageBox.Show("Another instance is already running");
-                Application.Current.Shutdown();
-                return;
-            }
-
 
             _logger.Debug ("=====================================================================================");
             _logger.Debug ("===                                 Start                                         ===");
@@ -96,6 +83,7 @@ namespace RdpManager {
 
             }
 
+            _viewModel = new MainViewModel();
             SetupSystemTray();
         }
 
@@ -104,8 +92,8 @@ namespace RdpManager {
         private void SetupSystemTray() {
             ContextMenuStrip _trayMenu = new ContextMenuStrip {
                 AutoClose = true,
-                ShowCheckMargin = false,
-                ShowImageMargin = false
+                ShowCheckMargin = false
+                // ShowImageMargin = false
             };
 
             _notifyIcon = new NotifyIcon {
@@ -340,7 +328,6 @@ namespace RdpManager {
         }
 
         protected override void OnClosing(CancelEventArgs e) {
-            _mutex.ReleaseMutex();
             _notifyIcon?.Dispose();
             base.OnClosing(e);
             Application.Current.Shutdown();
